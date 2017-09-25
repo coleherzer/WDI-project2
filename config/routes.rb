4c14baa9a3d5cd2524a31e412580a7eef656f78b
Cole Herzer <cole.herzer@gmail.com>
2017-09-25 16:16:18 -0700

    created controllers for exercises, diets, and goals

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#index' 
-
   resources :users
 
   resources :sessions, only: [:new, :create]
 
   delete '/logout' => 'sessions#destroy', as: :logout
+
+  resources :exercises
+  
 end
