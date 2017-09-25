a365371956dbf79fb14764e55622c4b26732a8fb
Cole Herzer <cole.herzer@gmail.com>
2017-09-25 15:56:49 -0700

    set up basic home page and set up basic sessions controller with early functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'users#index' 
 
   resources :users
+
+  resources :sessions, only: [:new, :create]
+
+  delete '/logout' => 'sessions#destroy', as: :logout
 end
