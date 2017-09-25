dde4f111a9d5ba236d9592ae1684a5bcb020fd48
Cole Herzer <cole.herzer@gmail.com>
2017-09-25 15:45:57 -0700

    corrected confirm password for new and edit pages, finished basic CRUD for users model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else 
+      redirect_to new_user_path
+    end
   end
 
   def edit
